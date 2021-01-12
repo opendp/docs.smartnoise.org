f45e77ad34b7bd04468d97df84b04414eeb181ea
Michael Phelan <mphelan@g.harvard.edu>
2021-01-12 12:23:30 -0500

    correction to pydata theme extension label

--- sphinx-opendp/conf.py
+++ sphinx-opendp/conf.py
@@ -29,7 +29,7 @@ release = '0.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'pydata-sphinx-theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'pydata_sphinx_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
